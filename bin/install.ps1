c1ee64af0083051b3bd61a1aef3d02325318933c
Stephane <stephane.cayssials@gmail.com>
2019-10-22 14:49:12 +0200

    Update install.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -36,13 +36,14 @@ iwr -useb get.scoop.sh | iex
 
 scoop install git
 Write-Host "Bootstrap git"
-Add-Content "$( scoop prefix git )/mingw64/ssl/certs/ca-bundle.crt" -Value (Get-Content -Path "$( scoop prefix devenv )/certs/axway.int.crt")
 Writ-Host ""
 $username = Read-Host -Prompt 'Prompt your axway Username for git.'
 git config --global user.name $username
 
 scoop bucket add devenv https://github.com/stephanec1/devenv-bucket.git
 scoop install devenv/devenv
+
+Add-Content "$( scoop prefix git )/mingw64/ssl/certs/ca-bundle.crt" -Value (Get-Content -Path "$( scoop prefix devenv )/certs/axway.int.crt")
 
 # Install di-conf by default
 devenv config --install --url https://git.ecd.axway.int/decisioninsight/hacking-week/hw-201910-devenv-configuration.git --name di-conf
